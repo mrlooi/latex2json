ec3fc05d18a61f870640df3cf693a48a44c1e82b
mrlooi <looicj@msn.com>
2025-01-03 14:27:43 +0800

    minor

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="latex_parser",
     version="0.1.0",
-    package_dir={"": "."},  # Changed from "src" to "." since package is in root
+    package_dir={"": "."},
     packages=find_packages(exclude=["tests*"]),
     include_package_data=True,
     install_requires=open("requirements.txt").read().splitlines(),
