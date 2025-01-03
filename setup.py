5827800da690a447444642f383b316f4bf2a4a4c
mrlooi <looicj@msn.com>
2025-01-03 13:15:59 +0800

    minor

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="latex-parser",
+    name="latex_parser",
     version="0.1.0",
     packages=find_packages(),
     install_requires=open("requirements.txt").read().splitlines(),
