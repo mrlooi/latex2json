e16b1e8473a4a062b8996ccbe0af9df3f5e984db
mrlooi <looicj@msn.com>
2025-01-03 12:54:22 +0800

    minor setup update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="tex-to-json",
+    name="latex-parser",
     version="0.1.0",
     packages=find_packages(),
     install_requires=open("requirements.txt").read().splitlines(),
