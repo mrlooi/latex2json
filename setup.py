31e8372a329ae777212ea0b2cb4c0358a1bba2de
mrlooi <looicj@msn.com>
2025-02-06 16:13:26 +0800

    update v0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="latex2json",
-    version="0.1.0",
+    version="0.2.0",
     package_dir={"": "."},
     packages=find_packages(exclude=["tests*"]),
     include_package_data=True,
