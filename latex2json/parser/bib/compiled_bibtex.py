10a9919e8654f899ad8136a8e5053bb8747295da
mrlooi <looicj@msn.com>
2025-03-14 23:10:19 +0800

    bugfix to compiled_bibtex to better handle brace content

--- latex2json/parser/bib/compiled_bibtex.py
+++ latex2json/parser/bib/compiled_bibtex.py
@@ -65,16 +65,18 @@ def parse_compiled_bibtex_entry(entry_text):
             # find the hash stuff
             for hash_match in re.finditer(r"\{hash=\w+\}\{", author_blocks):
                 hash_pos = hash_match.end() - 1
-                auth_block, end_pos = extract_nested_content(
+                auth_block, _ = extract_nested_content(
                     author_blocks[hash_pos:], "{", "}"
                 )
                 if auth_block:
                     # Extract family and given names
-                    family_match = re.search(r"family=\{([^}]+)\}", auth_block)
-                    given_match = re.search(r"given=\{([^}]+)\}", auth_block)
+                    family_match = re.search(r"(family=)\{([^}]+)\}", auth_block)
+                    given_match = re.search(r"(given=)\{([^}]+)\}", auth_block)
                     if family_match and given_match:
-                        family = family_match.group(1).strip()
-                        given = given_match.group(1).strip()
+                        start_pos = family_match.end(1)
+                        family, _ = extract_nested_content(auth_block[start_pos:])
+                        start_pos = given_match.end(1)
+                        given, _ = extract_nested_content(auth_block[start_pos:])
                         given = given[:1] + "."
                         authors.append(f"{family}, {given}")
 
@@ -82,9 +84,13 @@ def parse_compiled_bibtex_entry(entry_text):
             entry["author"] = " and ".join(authors)
 
     # Parse \field blocks (e.g. title, year, eprinttype, eprintclass)
-    for field_match in re.finditer(r"\\field\{([^}]+)\}\{([^}]+)\}", entry_text):
-        field_key = field_match.group(1).strip()
-        field_value = field_match.group(2).strip()
+    for field_match in re.finditer(r"(\\field)\{([^}]+)\}\{", entry_text):
+        start_pos = field_match.end(1)
+        blocks, _ = extract_nested_content_sequence_blocks(
+            entry_text[start_pos:], "{", "}", max_blocks=2
+        )
+        field_key = blocks[0].strip()
+        field_value = blocks[1].strip()
         # Skip internal fields that aren't needed.
         if field_key in ["labelnamesource", "labeltitlesource"]:
             continue
@@ -154,125 +160,26 @@ def is_compiled_bibtex(input_text: str) -> bool:
 
 if __name__ == "__main__":
     text = r"""
- \entry{deitke2023objaverse}{inproceedings}{}
-    \name{author}{10}{}{%
-      {{hash=DM}{%
-         family={Deitke},
-         familyi={D\bibinitperiod},
-         given={Matt},
-         giveni={M\bibinitperiod},
-      }}%
-      {{hash=SD}{%
-         family={Schwenk},
-         familyi={S\bibinitperiod},
-         given={Dustin},
-         giveni={D\bibinitperiod},
-      }}%
-      {{hash=SJ}{%
-         family={Salvador},
-         familyi={S\bibinitperiod},
-         given={Jordi},
-         giveni={J\bibinitperiod},
-      }}%
-      {{hash=WL}{%
-         family={Weihs},
-         familyi={W\bibinitperiod},
+   \entry{langsam2023}{misc}{}
+    \name{author}{1}{}{%
+      {{hash=ML}{%
+         family={Medeiros},
+         familyi={M\bibinitperiod},
          given={Luca},
          giveni={L\bibinitperiod},
       }}%
-      {{hash=MO}{%
-         family={Michel},
-         familyi={M\bibinitperiod},
-         given={Oscar},
-         giveni={O\bibinitperiod},
-      }}%
-      {{hash=VE}{%
-         family={VanderBilt},
-         familyi={V\bibinitperiod},
-         given={Eli},
-         giveni={E\bibinitperiod},
-      }}%
-      {{hash=SL}{%
-         family={Schmidt},
-         familyi={S\bibinitperiod},
-         given={Ludwig},
-         giveni={L\bibinitperiod},
-      }}%
-      {{hash=EK}{%
-         family={Ehsani},
-         familyi={E\bibinitperiod},
-         given={Kiana},
-         giveni={K\bibinitperiod},
-      }}%
-      {{hash=KA}{%
-         family={Kembhavi},
-         familyi={K\bibinitperiod},
-         given={Aniruddha},
-         giveni={A\bibinitperiod},
-      }}%
-      {{hash=FA}{%
-         family={Farhadi},
-         familyi={F\bibinitperiod},
-         given={Ali},
-         giveni={A\bibinitperiod},
-      }}%
-    }
-    \strng{namehash}{DMSDSJWLMOVESLEKKAFA1}
-    \strng{fullhash}{DMSDSJWLMOVESLEKKAFA1}
-    \field{labelnamesource}{author}
-    \field{labeltitlesource}{title}
-    \field{booktitle}{Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition}
-    \field{pages}{13142\bibrangedash 13153}
-    \field{title}{Objaverse: A universe of annotated 3d objects}
-    \field{year}{2023}
-  \endentry
-    
- \entry{levine2018learning}{article}{}
-    \name{author}{5}{}{%
-      {{hash=LS}{%
-         family={Levine},
-         familyi={L\bibinitperiod},
-         given={Sergey},
-         giveni={S\bibinitperiod},
-      }}%
-      {{hash=PP}{%
-         family={Pastor},
-         familyi={P\bibinitperiod},
-         given={Peter},
-         giveni={P\bibinitperiod},
-      }}%
-      {{hash=KA}{%
-         family={Krizhevsky},
-         familyi={K\bibinitperiod},
-         given={Alex},
-         giveni={A\bibinitperiod},
-      }}%
-      {{hash=IJ}{%
-         family={Ibarz},
-         familyi={I\bibinitperiod},
-         given={Julian},
-         giveni={J\bibinitperiod},
-      }}%
-      {{hash=QD}{%
-         family={Quillen},
-         familyi={Q\bibinitperiod},
-         given={Deirdre},
-         giveni={D\bibinitperiod},
-      }}%
     }
     \list{publisher}{1}{%
-      {SAGE Publications Sage UK: London, England}%
+      {GitHub}%
     }
-    \strng{namehash}{LSPPKAIJQD1}
-    \strng{fullhash}{LSPPKAIJQD1}
+    \strng{namehash}{ML1}
+    \strng{fullhash}{ML1}
     \field{labelnamesource}{author}
     \field{labeltitlesource}{title}
-    \field{number}{4-5}
-    \field{pages}{421\bibrangedash 436}
-    \field{title}{Learning hand-eye coordination for robotic grasping with deep learning and large-scale data collection}
-    \field{volume}{37}
-    \field{journaltitle}{The International journal of robotics research}
-    \field{year}{2018}
+    \field{howpublished}{\url{https://github.com/luca-medeiros/lang-segment-anything}}
+    \field{title}{Lang Segment Anything}
+    \field{journaltitle}{GitHub repository}
+    \field{year}{2023}
   \endentry
 
     """
